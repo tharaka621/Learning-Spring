d4f6519597db594bc2aa0215453c3a1e9f84477b
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-16 11:37:20 +0530

    Code Generator

---
+++ demo/src/main/java/ExtraKnowledge/codeGenerator.java
@@ -0,0 +1,10 @@
+package ExtraKnowledge;
+
+public class codeGenerator {
+
+	private String fName;
+	private String lName;
+	private String Address;
+
+	// Alt + Shift + S >>>> Generate Getters and setters / Constructor ... So on
+}
