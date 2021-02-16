ea489b0b84f766a3891a38bbf4269cf7fbeb0438
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-16 11:30:04 +0530

    Shortcut updated

--- demo/src/main/java/ExtraKnowledge/EclipseShortCuts.java
+++ demo/src/main/java/ExtraKnowledge/EclipseShortCuts.java
@@ -4,43 +4,46 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.print.attribute.HashAttributeSet;
-
 public class EclipseShortCuts {
 	private static String i;
 
-	public static void main(String[] args) { //'main' CTRL + SPACE
-		
-		//******** CTRL + SPACE ***************
-		BigDecimal bigDecimal = new BigDecimal(500.1); //Class name , CTRL + SPACE
-	
-		System.out.println(bigDecimal); // 'sout' 
-		System.err.println();   //syserr
-		
-		for (String string : args) { //fore		
+	public static void main(String[] args) { // 'main' CTRL + SPACE
+
+		// ******** CTRL + SPACE ***************
+		BigDecimal bigDecimal = new BigDecimal(500.1); // Class name , CTRL +
+														// SPACE
+
+		System.out.println(bigDecimal); // 'sout'
+		System.err.println(); // syserr
+
+		for (String string : args) { // fore
 		}
-		
-		
-		//******* CTRL + 1 ****************
-		//Rename the class name and change the project name
-		
-		
-		Integer integer = new Integer(i); //new Integer(i)
-		
-		//******** CTRL + SHIFT + L ************
-		
-			// To check the all the shortcuts
-		
-		//******** CTRL + SHIFT + R ************
-			//Search a specific file 
-		
-		//******** CTRL + SHIFT + T ************
-			//Search a specific Class 
-		
-		//******** Fn + F3 ************
-		
-		Map map = new HashMap();  // See the class. Class and hit the shortcut
-		map.put("key","value");  //see the map
+
+		// ******* CTRL + 1 ****************
+		// Rename the class name and change the project name
+
+		Integer integer = new Integer(i); // new Integer(i)
+
+		// ******** CTRL + SHIFT + L ************
+		// To check the all the shortcuts
+
+		// ******** CTRL + SHIFT + R ************
+		// Search a specific file
+
+		// ******** CTRL + SHIFT + T ************
+		// Search a specific Class
+
+		// ******** Fn + F3 ************
+
+		Map map = new HashMap(); // See the class. Class and hit the shortcut
+		map.put("key", "value"); // see the map
+
+		// ******** CTRL + SHIFT + F ************
+		// for formating the line
+
+		// ******** CTRL + SHIFT + O ************
+		// Remove unused IMPORTS
+
 	}
 
 }
