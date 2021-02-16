ffdc859705930ccbfeb8eea47c4697b51259ecb6
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-16 15:02:53 +0530

    Demo

---
+++ mavenDemoProject/src/main/java/com/MavenExample/mavenDemoProject/MavenDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MavenExample.mavenDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDemoProjectApplication.class, args);
+	}
+
+}
---
+++ mavenDemoProject/src/test/java/com/MavenExample/mavenDemoProject/MavenDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MavenExample.mavenDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
