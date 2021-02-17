9e172f314e63a27ee428213302c0029338a73b4f
tharaka621 <j.a.d.tharaka@gmail.com>
2021-02-17 20:44:13 +0530

    Use AutoWire in two Component instance

---
+++ demo/src/main/java/AutowireDemo01/BinarySearchImpl.java
@@ -0,0 +1,23 @@
+package AutowireDemo01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component // Tell Spring this is a Beans
+public class BinarySearchImpl {
+
+	@Autowired
+	private SortAlgotithm bubbleSortAlgorithm; // calling the implement by the
+												// class name. When you have two
+												// 'components' classes
+
+	public int binarSearch(int[] numbers, int numberToSearch) {
+
+		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+
+		// Search the array
+
+		return 3;
+	}
+
+}
---
+++ demo/src/main/java/AutowireDemo01/SortAlgotithm.java
@@ -0,0 +1,5 @@
+package AutowireDemo01;
+
+public interface SortAlgotithm {
+	public int[] sort(int[] numbers);
+}
